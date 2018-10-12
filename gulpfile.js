52b27c81e3ccdf82046517c6b100eaa61d883391
Phyu Phyu Thin <phyuphyuthinweb.it@gmail.com>
2018-10-12 15:11:37 +0630

    my first comming - admin views created

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var elixir = require('laravel-elixir');
+
+
+
+elixir(function (mix) {
+    var sass = mix.sass('app.scss');
+        //.webpack('app.js');
+});
+
+    .styles([
+        'libs/blog-post.css',
+        'libs/bootstrap.css/bootstrap.min.css',
+        'libs/font-awesome.css',
+        'libs/metisMenu.css',
+        'libs//style.css',
+        'libs/sb-admin-2.css',
+        'libs/timeline.css'
+
+    ], './public/css/libs.css')
+
+    .scripts([
+        'libs/bootstrap.js',
+        'libs/jquery.js',
+        'libs/metisMenu.js',
+        'libs/sb-admin-2.js',
+        'libs/scripts.js'
+
+    ], './public/js/libs.js')
+
